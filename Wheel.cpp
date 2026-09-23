65079efd7551af01c757b6f9a650aaab5fc02d4d
Kamel R <kamel.r@wayne.edu>
2026-09-23 17:41:46 -0400

    added the code for the Wheel class

--- Wheel.cpp
+++ Wheel.cpp
@@ -1,1 +1,28 @@
 #include "Wheel.h"
+#include <iostream>
+using namespace std; 
+
+void Wheel::setDiameter(float d)
+{
+	diameter = d; 
+}
+
+float Wheel::getDiameter()
+{
+	return diameter;
+}
+
+void Wheel::printInfo()
+{
+	cout << getDiameter() << endl;
+}
+
+Wheel::Wheel()
+{
+	setDiameter(14);
+}
+
+Wheel::Wheel(float d)
+{
+	setDiameter(d);
+}
